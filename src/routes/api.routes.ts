c6cf67d513d096771e589f6868682898c3edf3b1
carlos-burelo <carlosfernandoburelo@gmai.com>
2021-04-05 19:16:22 -0500

    Añadiendo ruta /years y /categories

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,6 +1,15 @@
 import { Router } from 'express';
 const routes = Router();
-import { getEmision, getLastest, getAnime, searchAnime, getEpisode, getGenders } from '../controllers/controller'
+import { 
+   getEmision, 
+   getLastest, 
+   getAnime, 
+   searchAnime, 
+   getEpisode, 
+   getGenders, 
+   getCategories, 
+   getYears 
+} from '../controllers/controller'
 
 
 routes.get('/lastest', (req, res) => {
@@ -20,6 +29,12 @@ routes.get('/search/:id', (req, res) => {
 });
 routes.get('/genders', (req, res) => {
    getGenders(req, res);
+});
+routes.get('/categories', (req, res) => {
+   getCategories(req, res);
+});
+routes.get('/years', (req, res) => {
+   getYears(req, res);
 });
 
 export default routes

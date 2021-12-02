ff69e9e00a29f4e3f1ca343a6ddef5185e5e415a
Angel <10299587+Darkangeel-hd@users.noreply.github.com>
2021-12-03 00:01:48 +0100

    Add correct port to starting log
    This prints the current listening port in the case it is a non default one

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import app from './app.js';
+const port = process.env.PORT || 5000;
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(port, () => {
   console.clear();
-  console.info(`API Running on: 5000 ============================`);
+  console.info(`API Running on: ${port} ============================`);
 });

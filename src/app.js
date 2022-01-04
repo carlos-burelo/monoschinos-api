ce5fc56b5576a7570aef6cea0df9183e4b277370
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-01-03 18:17:33 -0600

    disabling morgan

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-import morgan from 'morgan';
+// import morgan from 'morgan';
 import routes from './router.js';
 const app = express();
-app.use(cors(), morgan('dev'));
+// app.use(cors(), morgan('dev'));
 app.use('/', routes);
 export default app;

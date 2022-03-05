730b25c6cb8cdd87da6669dcbf5c9ebbb026554c
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-03-05 16:59:14 -0600

    :page_with_curl: add headers for all request

--- src/config.js
+++ src/config.js
@@ -1,3 +1,15 @@
+export const maskScrapingHeaders = {
+  'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+  Accept:
+    'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  'Accept-Language': 'es-ES,es;q=0.9,en;q=0.8',
+  'Accept-Encoding': 'gzip, deflate, br',
+  Connection: 'keep-alive',
+  'Upgrade-Insecure-Requests': '1',
+  'Cache-Control': 'max-age=0',
+};
+
 const url = 'https://monoschinos2.com';
 
 export const api = {

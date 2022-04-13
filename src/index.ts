c22e494e93be04db00318faf1e331a5caecc6eca
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-04-13 11:31:45 -0500

    :blue_square: add cors support

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import cors from 'cors'
 import express from 'express'
-import routes from './router'
 import { cache } from './api'
+import routes from './router'
 
 const app = express()
 
 app.use(cache)
+app.use(cors())
 app.use('/', routes)
 
 const port = process.env.PORT || 5000

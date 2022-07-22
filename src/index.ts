a5fd7ed4513919b321da6be6eb21bc9e1600c00f
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-07-22 10:50:21 -0500

    :wrench: chore: temporarily removed CORS support

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import apicache from 'apicache'
-import cors from 'cors'
 import express from 'express'
 import routes from './router'
 
 const app = express()
 const cache = apicache.middleware
 app.use(cache('5 seconds', (e: any) => console.log(e)))
-app.use(cors())
 app.use('/', routes)
 
 const port = process.env.PORT || 5000

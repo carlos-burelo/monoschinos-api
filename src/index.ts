255d55d3fea6776f9503c81bbfe9026e14882d57
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-07-22 11:09:45 -0500

    :recycle: refactor(runner): add configuration in the starting point of app

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import apicache from 'apicache'
-import express from 'express'
+import { App } from '@tinyhttp/app'
 import routes from './router'
+import { lruSend as cache } from 'lru-send'
+import { cors } from '@tinyhttp/cors'
 
-const app = express()
-const cache = apicache.middleware
-app.use(cache('5 seconds', (e: any) => console.log(e)))
+
+const app = new App()
+app.use(cache())
+app.use(cors())
 app.use('/', routes)
 
-const port = process.env.PORT || 5000
+const port: number = process.env.PORT ? parseInt(process.env.PORT) : 5000
 
-app.listen(port, () => {
-  console.clear()
-  console.info(`API Running on: ${port} ============================`)
-})
+app.listen(port, () => console.log(`Server running on port ${port}`))
